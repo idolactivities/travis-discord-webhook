82dbcbd5754d568b34c923d32a579c4434ec3c02
Musee Ullah <lae@lae.is>
2018-10-21 06:51:33 -0700

    Validate that payloads are legitimate

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import base64
 import json
 import logging
 
 from flask import Flask, request, abort
+from OpenSSL.crypto import verify, load_publickey, FILETYPE_PEM, X509
+from OpenSSL.crypto import Error as SignatureError
 import requests
 import yaml
 
@@ -15,6 +18,7 @@ with open("config.yaml") as file:
 
 DISCORD_WEBHOOK = config["discord_webhook"]
 AUTHORIZED_OWNERS = config["authorized_owners"]
+TRAVIS_CONFIG_URL = "https://api.travis-ci.org/config"
 COLORS = config["colors"]
 
 
@@ -22,10 +26,16 @@ app = Flask(__name__)
 
 @app.route("/notify", methods=["POST"])
 def webhook():
-    data = request.form["payload"]
-    data = json.loads(data)
+    raw_data = request.form["payload"]
+    signature = base64.b64decode(request.headers.get("Signature"))
+    data = json.loads(raw_data)
 
     if data["repository"]["owner_name"] not in AUTHORIZED_OWNERS:
+        abort(401)
+
+    try:
+        check_authorized(signature, PUBLIC_KEY, raw_data)
+    except SignatureError:
         abort(401)
 
     # Force lower because yaml uses lower case
@@ -87,6 +97,26 @@ def server_error(e):
     sorry
     """.format(e), 500
 
+# https://gist.github.com/andrewgross/8ba32af80ecccb894b82774782e7dcd4
+def get_travis_public_key():
+    """
+    Returns the PEM encoded public key from the Travis CI /config endpoint
+    """
+    response = requests.get(TRAVIS_CONFIG_URL, timeout=10.0)
+    response.raise_for_status()
+    return response.json()['config']['notifications']['webhook']['public_key']
+
+def check_authorized(signature, public_key, payload):
+    """
+    Convert the PEM encoded public key to a format palatable for pyOpenSSL,
+    then verify the signature
+    """
+    pkey_public_key = load_publickey(FILETYPE_PEM, public_key)
+    certificate = X509()
+    certificate.set_pubkey(pkey_public_key)
+    verify(certificate, signature, payload, str('sha1'))
+
+PUBLIC_KEY = get_travis_public_key()
 
 if __name__ == "__main__":
     app.run(debug=True)
